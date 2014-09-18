2bd1bdf9beff5f02c9e475e1a19484f48dffe597
shellygr <shelly6.g@gmail.com>
2014-09-18 22:01:35 +0300

    First
    Basic functionality for all (current) needs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileClassifier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] files;
+        int numOfRelevantFiles;
+        string currentFile;
+        int currentFileNumberWorkingOn = 0;
+        string outputFilename;
+        string lastDescriptionText;
+
+        string[] mediaExtensions = { "mp3", "mp4", "wav", "3ga", "avi", "flv", "wmv", "wma", "flac" };
+        string[] imageExtensions = { "jpg", "jpeg", "gif", "bmp", "png" };
+
+        AxAXVLC.AxVLCPlugin vlcPlayer = new AxAXVLC.AxVLCPlugin();
+        public MainWindow()
+        {
+            InitializeComponent();
+            mediaPreview.Child = vlcPlayer;
+            path.Text = "C:\\Users\\";
+        }
+
+        private void browse(object sender, RoutedEventArgs e)
+        {
+            //Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            System.Windows.Forms.FolderBrowserDialog dlg = new System.Windows.Forms.FolderBrowserDialog();
+            dlg.RootFolder = System.Environment.SpecialFolder.MyDocuments;
+            dlg.ShowNewFolderButton = false;
+            
+            // Set filter for file extension and default file extension 
+            //dlg.DefaultExt = ".jpg";
+            //dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            System.Windows.Forms.DialogResult result = dlg.ShowDialog();
+            
+            // Get the selected file name and display in a TextBox 
+            if (result.Equals(System.Windows.Forms.DialogResult.OK))
+            {
+                string pathName = dlg.SelectedPath;
+                path.Text = pathName;
+            }
+        }
+
+
+        private void calcNumOfRelevantFiles()
+        {
+            string[] relevantExtensions = {};
+            relevantExtensions = relevantExtensions.Concat(imageExtensions).ToArray();
+            relevantExtensions = relevantExtensions.Concat(mediaExtensions).ToArray();
+
+            //MessageBox.Show(String.Join(",", imageExtensions), "relevant extensions", MessageBoxButton.OK);
+            //MessageBox.Show(String.Join(",", mediaExtensions), "relevant extensions", MessageBoxButton.OK);
+            //MessageBox.Show(String.Join(",", relevantExtensions), "relevant extensions", MessageBoxButton.OK);
+
+            int counter = 0;
+
+            foreach (string file in files)
+            {
+                string ext = file.Split('.').Last();
+                if (relevantExtensions.Contains(ext))
+                {
+                    //MessageBox.Show("Relevant extension " + ext + " found", "Found relevant extension", MessageBoxButton.OK);
+                    ++counter;
+                }
+                else
+                {
+                    MessageBox.Show("Extension" + ext + " is not relevant", "Extension not relevant", MessageBoxButton.OK);
+                }
+            }
+
+            numOfRelevantFiles = counter;
+        }
+
+        // TODO: numOfRelevantFiles will contain only the new files not yet reviewed
+        private void fetchFiles(object sender, RoutedEventArgs e)
+        {
+            files = System.IO.Directory.GetFiles(path.Text, "*", System.IO.SearchOption.TopDirectoryOnly);
+            calcNumOfRelevantFiles();
+            outputFilename = path.Text + "\\folderDescription_" + DateTime.Now.ToString("dd_MM_yyyy_hh_mm");
+
+            counter.Content = currentFileNumberWorkingOn + "/" + numOfRelevantFiles;
+
+            if (numOfRelevantFiles == 0)
+            {
+                MessageBox.Show("Nothing to work on - no relevant files", "Error");
+                return;
+            }
+
+            workOnCurrentFile();
+            //MessageBox.Show(outputFilename, "path", MessageBoxButton.OK);
+        }
+        
+        private void workOnCurrentFile()
+        {
+            currentFile = files[currentFileNumberWorkingOn];
+            currentFileGUIBox.Content = currentFile;
+            description.Focus();
+
+            string ext = currentFile.Split('.').Last();
+            if (imageExtensions.Contains(ext))
+            {
+                preview.Source = new BitmapImage(new Uri(currentFile));
+            }
+            else if (mediaExtensions.Contains(ext))
+            {
+                vlcPlayer.stop();
+                vlcPlayer.playlistClear();
+                vlcPlayer.addTarget("file:///" + currentFile, null, AXVLC.VLCPlaylistMode.VLCPlayListReplaceAndGo, 0);
+                vlcPlayer.play();
+            }
+            else
+            {
+                //MessageBox.Show("Got irrelevant file " + currentFile + ", Skipping");
+                ++currentFileNumberWorkingOn;
+                workOnCurrentFile();
+            }
+        }
+
+        // TODO: Add support for update mechanism. That is, take the last description file, and the new file will be the same but updated - only new files will be added.
+        private void submit(object sender, RoutedEventArgs e)
+        {
+            using (System.IO.StreamWriter outputFile = new System.IO.StreamWriter(outputFilename, true))
+            {
+                outputFile.WriteLine(currentFile + "," + description.Text);
+                ++currentFileNumberWorkingOn;
+            }
+
+            lastDescriptionText = description.Text;
+            description.Text = "";
+
+            if (currentFileNumberWorkingOn > numOfRelevantFiles)
+            {
+                MessageBox.Show("Done!\nChoose another folder.", "FileClassifier");
+                return;
+            }
+
+            counter.Content = currentFileNumberWorkingOn + "/" + numOfRelevantFiles;
+            workOnCurrentFile();
+        }
+
+        // TODO: full history
+        private void description_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Up))
+            {
+                description.Text = lastDescriptionText;
+            }
+        }
+    }
+}
---
+++ vlc.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FileClassifier
+{
+    partial class vlc
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(vlc));
+            this.axVLCPlugin1 = new AxAXVLC.AxVLCPlugin();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axVLCPlugin1
+            // 
+            this.axVLCPlugin1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axVLCPlugin1.Enabled = true;
+            this.axVLCPlugin1.Location = new System.Drawing.Point(0, 0);
+            this.axVLCPlugin1.Name = "axVLCPlugin1";
+            this.axVLCPlugin1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin1.OcxState")));
+            this.axVLCPlugin1.Size = new System.Drawing.Size(150, 150);
+            this.axVLCPlugin1.TabIndex = 0;
+            // 
+            // vlc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.axVLCPlugin1);
+            this.Name = "vlc";
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin axVLCPlugin1;
+    }
+}
